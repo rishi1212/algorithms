f0f15bbcb58155ca22663e96d96faab8aef431d3
rishi <me.rishiverma@gmail.com>
2016-08-20 15:36:36 +0530

    test

---
+++ test/src/test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Rishi on 20/08/16.
+ */
+
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.print("test here");
+
+    }
+}
